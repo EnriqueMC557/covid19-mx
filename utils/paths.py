fb0d5e2368461200cad5aa1184d35911c8baa2bb
Enrique Mena-Camilo <kikemeca05@gmail.com>
2023-05-22 20:21:31 -0600

    download_dataset

---
+++ utils/paths.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Contains project paths.
+"""
+
+import os
+
+
+PROJECT_PATH = os.path.dirname(os.path.dirname(__file__))
+DATA_PATH = PROJECT_PATH + "/data"
+FIGURES_PATH = PROJECT_PATH + "/figures"
+MODELS_PATH = PROJECT_PATH + "/models"
